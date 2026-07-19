76b45fd7cebd72b93eda9212ddf44576fdfa53e4
kazu <Walk.aaaiaua@gmail.com>
2026-07-20 08:52:55 +0900

    デプロイ対応2

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dailyreport-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
